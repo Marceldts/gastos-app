1d92a1c3a921e944bc4a093ea8fa0c41f387a85d
Marceldts <marceldeltoro23@gmail.com>
2024-01-27 18:38:49 +0100

    created group validators and tests changed compiler target to es6

---
+++ src/test/domain/group/Group.test.tsx
@@ -0,0 +1,42 @@
+import { User } from "domain/user/User";
+import { Group, isGroupValid } from "domain/group/Group";
+
+describe('isGroupValid', () => {
+    test('valid group', () => {
+        const validMockedGroup: Group = {
+            expenseList: new Set(),
+            members: new Set()
+        };
+        expect(() => isGroupValid(validMockedGroup)).not.toThrow();
+    });
+});
+
+describe('isGroupNotValid', () => {
+    beforeEach(() => {
+        let errorMessage = "";
+    });
+    test('members list has duplicate ids', () => {
+        const invalidUser1: User = {
+            id: 1,
+            name: "Test user 1",
+            balance: 0
+        };
+        const invalidUser2: User = {
+            id: 1,
+            name: "Test user 2",
+            balance: 0
+        };
+        const invalidMockedGroup: Group = {
+            expenseList: new Set(),
+            members: new Set([invalidUser1, invalidUser2])
+        };
+        expect(() => isGroupValid(invalidMockedGroup)).toThrow("\nMembers list has duplicate ids.\n");
+    });
+    test('cannot have expenses without members', () => {
+        const invalidMockedGroup: Group = {
+            expenseList: new Set([{ payerId: 1, amount: 100, description: 'Test expense', date: '2022-01-28' }]),
+            members: new Set()
+        };
+        expect(() => isGroupValid(invalidMockedGroup)).toThrow("\nCannot have expenses without members.\n");
+    });
+});
