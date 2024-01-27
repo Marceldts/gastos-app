aee1d0f23cf298855f785b130c6d04e07ea05a62
Marceldts <marceldeltoro23@gmail.com>
2024-01-27 17:55:57 +0100

    changed group attr from array to set

--- src/domain/group/Group.tsx
+++ src/domain/group/Group.tsx
@@ -2,6 +2,6 @@ import { Expense } from "@domain/expense/Expense";
 import { User } from "@domain/user/User";
 
 export interface Group {
-    expenseList: Expense[],
-    members: User[]
+    expenseList: Set<Expense>,
+    members: Set<User>
 }
