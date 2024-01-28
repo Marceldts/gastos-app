8fe2e1b174d6e4632e05966aa8a067c4bf4bdf31
Marceldts <marceldeltoro23@gmail.com>
2024-01-28 16:41:51 +0100

    localstorage group repository and test

--- src/infrastructure/group/LocalStorageGroup.repository.tsx
+++ src/infrastructure/group/LocalStorageGroup.repository.tsx
@@ -1,22 +1,72 @@
-import { Expense } from "@domain/expense/Expense";
-import { Group } from "@domain/group/Group";
-import { GroupRepository } from "@domain/group/Group.repository";
-import { User } from "@domain/user/User";
+import { Expense } from "domain/expense/Expense";
+import { Group, isGroupValid } from "domain/group/Group";
+import { GroupRepository } from "domain/group/Group.repository";
+import { User } from "domain/user/User";
 
 export const localStorageGroupRepository: GroupRepository = {
+    //We have to convert the Arrays to Sets because the Set object is not serializable, and thus, cannot be saved in localStorage.
     getGroup: function (): Promise<Group | undefined> {
-        throw new Error("Function not implemented.");
+        return new Promise((resolve, reject) => {
+            try {
+                const stringifiedGroup = localStorage.getItem('group');
+                if (stringifiedGroup) {
+                    const parsedGroup = JSON.parse(stringifiedGroup);
+                    parsedGroup.members = new Set(parsedGroup.members);
+                    parsedGroup.expenseList = new Set(parsedGroup.expenseList);
+                    resolve(parsedGroup);
+                } else {
+                    const emptyGroup: Group = {
+                        expenseList: new Set(),
+                        members: new Set(),
+                    };
+                    this.saveGroup(emptyGroup).then(() => resolve(emptyGroup));
+                }
+            } catch (error) {
+                reject(error);
+            }
+        });
     },
+
+    //We have to convert the Sets to Arrays because the Set object is not serializable, and thus, cannot be saved in localStorage.
     saveGroup: function (group: Group): Promise<void> {
-        throw new Error("Function not implemented.");
+        return new Promise((resolve, reject) => {
+            try {
+                const groupToSave = {
+                    expenseList: Array.from(group.expenseList),
+                    members: Array.from(group.members),
+                };
+                localStorage.setItem('group', JSON.stringify(groupToSave));
+                resolve();
+            } catch (error) {
+                reject(error);
+            }
+        });
     },
     addExpense: function (group: Group, expense: Expense): Promise<void> {
-        throw new Error("Function not implemented.");
+        return new Promise((resolve, reject) => {
+            try {
+                group.expenseList.add(expense);
+                this.saveGroup(group).then(() => resolve());
+            } catch (error) {
+                reject(error);
+            }
+        });
     },
     addMember: function (group: Group, member: User): Promise<void> {
-        throw new Error("Function not implemented.");
+        return new Promise((resolve, reject) => {
+            try {
+                group.members.add(member);
+                this.saveGroup(group).then(() => resolve());
+            } catch (error) {
+                reject(error);
+            }
+        });
     },
-    getGroupBalance: function (group: Group): Promise<Map<User, number>> | Promise<null> {
-        throw new Error("Function not implemented.");
+    getGroupBalance: function ({ members }: Group): Promise<Map<User, number>> | Promise<null> {
+        const groupBalance = new Map<User, number>();
+        for (const member of members) {
+            groupBalance.set(member, member.balance);
+        }
+        return Promise.resolve(groupBalance);
     }
 }
