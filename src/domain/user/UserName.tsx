f3e3925461b1da734acbf532e5637db67b419e40
Marceldts <marceldeltoro23@gmail.com>
2024-01-28 10:56:45 +0100

    implemented 50 character limit to username refactored isUserNameValid into different atomic functions

--- src/domain/user/UserName.tsx
+++ src/domain/user/UserName.tsx
@@ -1,4 +1,16 @@
 export const isUserNameValid = (name: string): boolean => {
+    return nameIsNotEmpty(name) && nameIsNotMoreThan50Characters(name) && nameMatchesRegex(name);
+}
+
+const nameMatchesRegex = (name: string): boolean => {
     const regex = /^[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚüÜçÇ\s]+$/
-    return name.trim().length > 0 && regex.test(name.trim());
+    return regex.test(name.trim());
+}
+
+const nameIsNotEmpty = (name: string): boolean => {
+    return name.trim().length > 0;
+}
+
+const nameIsNotMoreThan50Characters = (name: string): boolean => {
+    return name.trim().length <= 50;
 }
