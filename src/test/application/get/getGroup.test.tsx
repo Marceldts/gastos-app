bd4469f26e7ec4166e155187edbe79a2511bd288
Marceldts <marceldeltoro23@gmail.com>
2024-01-30 17:16:24 +0100

    fixed getGroup test

--- src/test/application/get/getGroup.test.tsx
+++ src/test/application/get/getGroup.test.tsx
@@ -3,14 +3,8 @@ import { groupRepositoryMock } from "test/mocks/groupRepository.mock";
 
 describe('getGroup', () => {
     const repository = groupRepositoryMock;
-    let getItemMock: jest.SpyInstance;
-
-    beforeAll(() => {
-        getItemMock = jest.spyOn(Storage.prototype, 'getItem');
-    });
 
     test('getGroup should call the repository getGroup method method', async () => {
-        getItemMock.mockReturnValueOnce(JSON.stringify(undefined));
         await getGroup(repository);
         expect(groupRepositoryMock.getGroup).toHaveBeenCalledTimes(1);
     });
