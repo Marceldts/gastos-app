11f8b9e05e5285d0acd7dd01729fd84aa1e89648
Marceldts <marceldeltoro23@gmail.com>
2024-01-31 19:12:59 +0100

    getGroupBalance and test

---
+++ src/application/get/getGroupBalance.tsx
@@ -0,0 +1,12 @@
+import { Group } from "@domain/group/Group";
+import { GroupRepository } from "@domain/group/Group.repository";
+
+export const getGroupBalance = async (groupRepository: GroupRepository, group: Group) => {
+    try {
+        const groupBalance = await groupRepository.getGroupBalance(group);
+        return Promise.resolve(groupBalance);
+    }
+    catch (error) {
+        return Promise.reject(error);
+    }
+}
